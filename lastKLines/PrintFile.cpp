d49c0e28ab81d6122352721db47e371cf592cdb9
alex <alex.tran234@gmail.com>
2016-10-20 17:17:54 +0800

    move private class into cpp file

--- lastKLines/PrintFile.cpp
+++ lastKLines/PrintFile.cpp
@@ -2,6 +2,35 @@
 
 using namespace std;
 
+template<typename T>
+class PrintFile::CircularBuffer {
+public:
+  
+  CircularBuffer ()=delete;
+  CircularBuffer (int capacity) :_buf{new T[capacity]},_idx{0}, _capacity(capacity) {}
+  
+  void push (const T& item){
+    _buf[(_idx++)%_capacity] = item;
+  }
+  
+  const T& operator[] (size_t idx) const { return _buf[idx];}
+  T& operator[] (size_t idx) { return _buf[idx];}
+  
+  ~CircularBuffer(){
+    delete[]  _buf;
+  }
+  
+  size_t lastIdx () const { return _idx;}
+  
+  
+private:
+  T *_buf;
+  int _idx;
+  int _capacity;
+};
+
+
+
 PrintFile::PrintFile (std::string filename) : f(filename) {
   
 }
--- lastKLines/PrintFile.h
+++ lastKLines/PrintFile.h
@@ -20,34 +20,10 @@ public:
 private:
   
   std::ifstream f;
-
-  template<typename T>
-  class CircularBuffer {
-  public:
-    
-    CircularBuffer ()=delete;
-    CircularBuffer (int capacity) :_buf{new T[capacity]},_idx{0}, _capacity(capacity) {}
-    
-    void push (const T& item){
-      _buf[(_idx++)%_capacity] = item;
-    }
-    
-    const T& operator[] (size_t idx) const { return _buf[idx];}
-    T& operator[] (size_t idx) { return _buf[idx];}
-    
-    ~CircularBuffer(){
-      delete[]  _buf;
-    }
-    
-    size_t lastIdx () const { return _idx;}
-    
-    
-  private:
-    T *_buf;
-    int _idx;
-    int _capacity;
-  };
   
+  template<typename T>
+  class CircularBuffer;
+
 };
 
 #endif //__PRINTFILE__
