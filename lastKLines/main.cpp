6e25367991505e33fdc52f23a4c9b02802cd0e91
alex <alex.tran234@gmail.com>
2016-10-20 15:55:20 +0800

    first try

---
+++ lastKLines/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void printLastKLines (string filename, int k)  {
+  
+  ifstream f(filename);
+  
+  if (!f.is_open()) return;
+  
+  string *kLines{new string[k]};
+  int totalLines{0};
+  int currentLine{0};
+  
+  string line;
+  // read file line by line
+  while (!f.eof()){
+    getline(f, line);
+    kLines[currentLine] = line;
+    currentLine = (currentLine+1)%k;
+    ++totalLines;
+  }
+  f.close();
+  
+  
+  // print the results
+  int startLine = (totalLines <k)?0: currentLine;
+  for (int i=0; i<k;i++){
+    cout << kLines[(startLine+i)%k]<<endl;
+  }
+  delete[]  kLines;
+  
+  
+}
+
+int main() {
+  printLastKLines("fileNotExist.txt", 3);
+  cout <<endl<<endl;
+  printLastKLines("../test1.txt", 3);
+  cout <<endl<<endl;
+  return 0;
+    
+ }
