792695fb6d1db033c3731c1de8238cedd5551029
alex <alex.tran234@gmail.com>
2016-10-21 09:34:57 +0800

    adding googlemock for testing

---
+++ lastKLines/testPrintFile.cpp
@@ -0,0 +1,14 @@
+#include "gmock/gmock.h"    
+#include "PrintFile.h"
+
+TEST(TestPrintFile, InputFileDoesNotExist) { 
+   
+   
+}
+
+
+
+int main(int argc, char** argv) {
+   testing::InitGoogleMock(&argc, argv);
+   return RUN_ALL_TESTS();
+}
